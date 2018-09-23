48deee76a7c6877f7234b8117cebef59586522fb
Thomas Gartman <thomasgartman@ku.edu>
2018-09-23 14:06:54 -0500

    Bad music documentation

---
+++ js/music.js
@@ -0,0 +1,11 @@
+/**
+* @fileOverview This class will randomly plays a song in the sound file
+* whenever required
+* @param {boolean} state - If rock music needs to be played or not
+*	@pre: This code needs to be ran after the page is rendered!
+* @post: Music Plays or Doesn't Play
+*/
+function music(state)
+{
+	
+}
