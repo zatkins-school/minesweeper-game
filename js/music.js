0eb2074a9c2824eb17e3b7c0f2cd1b6280fb68d4
Thomas Gartman <thomasgartman@ku.edu>
2018-09-23 14:39:35 -0500

    Finished version 1 of the music class. Time for testing.

--- js/music.js
+++ js/music.js
@@ -5,23 +5,55 @@
 
 /**
 	*	@class
-	*	This class will randomly play a song in the sound folder whenever required.
-	* @property {boolean} state - If Rock music needs to be played or not.
-	* @property {string} src - Music source string
+	*	This class will play songs in the sound folder whenever required.
+	* @property {boolean} state - State True = Music plays. State False = No Music.
 */
 
 /**
 	* @constructor
+	* The this.music syntax is to be used with the HTML5 audio tag
 	*	@pre: This code needs to be ran after the page is rendered!
 	* @post: Music Plays or Doesn't Play
 	* @return: Nothing
 */
 function music(state)
 {
+	this.music = document.createElement("audio");
+	this.music.src = "../sound/*.mp3";
+	this.music.setAttribute("preload", "auto");
+	this.music.setAttribute("controls", "none");
+	this.music.style.display = "none";
+	document.body.appendChild(this.sound);
 	this.state = state;
-	this.src = "../sound/";
 }
 
 /**
-	* @
+	* @function
+	* This function will manage whether or not to play a song
+	* @pre: None
+	* @post: Mus
 */
+function toggleMusic()
+{
+	state = !(state);
+}
+
+/**
+	* @function
+	* This function will choose a song and play it if the state is true
+	* It will also turn off the music if the state is false
+	* @pre: Music is in the src location
+	* @post: Music will play
+	* @return: Nothing
+*/
+function playMusic()
+{
+	if (state)
+	{
+		this.music.play();
+	}
+	else
+	{
+		this.music.pause();
+	}
+}
