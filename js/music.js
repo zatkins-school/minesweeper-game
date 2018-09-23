e92aac5a2bfc8f6cb13c63a4340e96baafff5de5
Thomas Gartman <thomasgartman@ku.edu>
2018-09-23 14:14:27 -0500

    Music Constructor

--- js/music.js
+++ js/music.js
@@ -1,11 +1,27 @@
 /**
-* @fileOverview This class will randomly plays a song in the sound file
-* whenever required
-* @param {boolean} state - If rock music needs to be played or not
-*	@pre: This code needs to be ran after the page is rendered!
-* @post: Music Plays or Doesn't Play
+	* @fileOverview Manages Music
+	* whenever required
+*/
+
+/**
+	*	@class
+	*	This class will randomly play a song in the sound folder whenever required.
+	* @property {boolean} state - If Rock music needs to be played or not.
+	* @property {string} src - Music source string
+*/
+
+/**
+	* @constructor
+	*	@pre: This code needs to be ran after the page is rendered!
+	* @post: Music Plays or Doesn't Play
+	* @return: Nothing
 */
 function music(state)
 {
-	
+	this.state = state;
+	this.src = "../sound/";
 }
+
+/**
+	* @
+*/
