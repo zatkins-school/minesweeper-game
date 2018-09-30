e65b75b79f6711cb251852d550f6f0ace547aaa1
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-30 16:24:45 -0500

    added logic to set a checkbox value based on toggle button state

--- js/toggleButton.js
+++ js/toggleButton.js
@@ -1,10 +1,15 @@
 function toggleIntenseMode() {
     let button = document.getElementById("intenseMode");
+    let checkBox = document.getElementById
+    ("intenseModeInput");
     if (button.innerText==="Normal Mode") {
         button.innerText = "INTENSE MODE!!";
         button.style.backgroundColor = "red";
+        checkBox.checked = true;
     } else {
         button.innerText = "Normal Mode";
         button.style.backgroundColor = "#ffce00";
+        checkBox.checked = false;
     }
+    console.log(checkBox.checked);
 }
